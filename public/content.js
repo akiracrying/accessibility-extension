35c59d29c55dbc86c53ed42ddbd0386c6508f5c5
Тимофей Цветков <tsvetkov.ti@edu.spbstu.ru>
2023-09-23 20:47:42 +0300

    [feature] Handler

--- public/content.js
+++ public/content.js
@@ -1,9 +1,9 @@
-
+import {mouseMoverOn, mouseMoverOff} from './mouse_mover'
 
 function eventChecker(elem) {
     console.log("")
     for (const key in elem) {
-        if(/^on/.test(key)) {
+        if (/^on/.test(key)) {
             const eventType = key.substr(2);
             elem.addEventListener(eventType, (evt) => {
                 console.log(evt);//evt.type);
@@ -12,13 +12,27 @@ function eventChecker(elem) {
     }
 }
 
-browser.runtime.onMessage.addEventListener(data => {
+let conditions = {
+    mouse: 0,
+    contrast: 0,
+    font: 0,
+    optimizer: 0,
+    speaker: 0
+}
+
+browser.runtime.onMessage.addListener(data => {
     const {
         trigger,
 
     } = data;
-    switch (trigger){
+    switch (trigger) {
         case "mouseKeyboard":
+            if (conditions.mouse === 0) {
+                conditions.mouse = 1
+                mouseMoverOn()
+            } else {
+                mouseMoverOff()
+            }
             mouseMover()
             break
         case "highContast":
